76cdf3cfc8bb3b22d1554d8e4b4251728c2c2504
Altria4933 <junchen4933@gmail.com>
2021-07-04 15:46:45 +1200

    Added token authorize service

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,12 +21,14 @@ namespace API.Controllers
 
         //return all users
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){ 
             //asynchronous method to aid performance
             return await _context.Users.ToListAsync();
         }
 
-        // return a specfic user
+        // return a specific user
+        [Authorize]
         [HttpGet("{id}")]
         public async Task<ActionResult<AppUser>> GetUsers(int id){
             return await _context.Users.FindAsync(id);
